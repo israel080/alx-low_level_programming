59e0261ef8bed7e22c81ec7e907edd83ce51df2e
israel080 <promisecharles90@gmail.com>
2022-09-12 21:46:03 -0700

    print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -print the num from 0 to 9
+ *
+ * Return:Alaways 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	puchar ('\n');
+
+	return (0);
+
+}
+
