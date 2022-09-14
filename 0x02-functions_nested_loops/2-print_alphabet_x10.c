b4339a1202a8a5c592059497e1034944cea3b29b
israel080 <promisecharles90@gmail.com>
2022-09-14 05:12:43 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet_x10.c(void)
 {
 	int c, i;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = 'a'; c <= 'z'; i++)
 		{
