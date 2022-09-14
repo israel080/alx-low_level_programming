91d570e0e426e4e1cea5ca7674921eb64f55c6ef
israel080 <promisecharles90@gmail.com>
2022-09-14 11:13:33 -0700

    islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,14 @@
  *@c: is the int that will be use for the argument of the function
  * Return: Always 0 (Success)
  */
-int_islower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 	else
+	{
 		return (0);
+	}
 }
