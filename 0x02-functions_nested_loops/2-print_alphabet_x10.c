64b311ce988cf1eedfc49eb47d3f344881698d84
israel080 <promisecharles90@gmail.com>
2022-09-14 04:05:48 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -function that prints 10 times the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (i = 'a'; c <= 'z'; i++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
