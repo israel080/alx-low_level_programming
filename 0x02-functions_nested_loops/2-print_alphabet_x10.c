af69e3857eda6c90c2462b86ea0a6e1f1fe3ce90
israel080 <promisecharles90@gmail.com>
2022-09-15 12:11:19 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints the lowercae alphabet 10 times on news line
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char ='a';
+	int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(c + j);
+		}
+		_putchar(10);
+	}
+}
