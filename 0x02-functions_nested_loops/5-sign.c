89284895e1a6f5615a3f58c48e876f02ac8a5d53
israel080 <promisecharles90@gmail.com>
2022-09-15 00:59:22 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 
 /**
  *print_sign - prints the sign of a number
- * @c: character to print positive, zero or negarive integer
- * Return: 1
- * describe: if it's positive, 0 if it's negative and -1 if it's negative
+ *@n: character to print positive, zero or negarive integer
+ *Return: 1
+ *describe: if it's positive, 0 if it's negative and -1 if it's negative
  */
 
 int print_sign(int n)
