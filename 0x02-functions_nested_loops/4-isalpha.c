0c9c073e06923b16c05c768dc5efbb25fd85f274
israel080 <promisecharles90@gmail.com>
2022-09-15 01:07:07 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,12 @@
 
 /**
  *_isalpha.c - function to checks for alphabetic character
- *
  *@c:the character to print
- *
  *Return: Always 0
  *on fail -1 and errno is set appropriately
  */
 
 int _isalpha(int c)
-
 {
 	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
 	{
