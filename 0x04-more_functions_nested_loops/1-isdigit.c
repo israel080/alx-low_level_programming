d1e9d5a3cbffc7c7fe761a2256e1e0f2ad354590
israel080 <promisecharles90@gmail.com>
2022-09-16 21:59:11 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_isdigit - function to check zero to 9
+ *@c: check input function
+ *Return: 1 if it is,otherwise always 0
+ */
+
+int _isdigit(int c)
+{
+	if ((c <= 48) && (c >= 57))
+	{
+		return (i);
+	}
+	return (0);
+}
