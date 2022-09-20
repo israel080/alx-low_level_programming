92c60f7bb5d15468bbd22ad4702295056ac7bd3c
israel080 <promisecharles90@gmail.com>
2022-09-20 11:36:21 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string.
+ * stdout
+ *
+ * @str: To input stringd
+ *
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0' index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
