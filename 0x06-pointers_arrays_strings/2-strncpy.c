30cabef5516e6b8c851cc7bec7b4f0d1debf4e6d
israel080 <promisecharles90@gmail.com>
2022-09-21 22:12:33 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,9 +9,15 @@
  * Return: string
  */
 
-char *_strncpy(char *dest, char *src, int 
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-	strcpy(dest, src);
+	for (i = 0; i < n && src[i] != '\0' i++)
+		dest [i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] ='\0';
+
 	return (dest);
 }
