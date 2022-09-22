986213a8777c7c231ee6318939008d44665760d0
israel080 <promisecharles90@gmail.com>
2022-09-21 21:44:15 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,19 @@
 
 char *_strncat(char *dest, char *src, int n);
 {                                                                              
-	int index = 0;
-	dest_len = 0;
+	int dlen = 0, j = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	while  (j < n && src[j]);
+	{
+		dest[dlen] = src[j];
+		dlen++;
+		j++;
+	}
+	dest[dlen + n + 1] = '\0';
 
 	return (dest);
 }          
