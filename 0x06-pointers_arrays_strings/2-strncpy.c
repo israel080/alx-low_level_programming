77860d8a44885de5cb4914565c6ea4825670c1df
israel080 <promisecharles90@gmail.com>
2022-09-21 20:48:10 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - function for copying purpose
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strcmp(dest, src, n);
+	return (dest);
+}
